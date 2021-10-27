e4a886a5e8fa1f22b13cbc7736c51f395b17e0d0
Dimitris <jvortelinas@gmail.com>
2021-10-27 13:48:36 +0200

    First Commit

--- login_system/reset-password.php
+++ login_system/reset-password.php
@@ -79,7 +79,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
         body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
+        .wrapper{ width: 350px; padding: 20px; margin: 200px 0 0 800px; }
     </style>
 </head>
 <body>
